0a6fead1ccc432b3fa06cca3189d1d4d1e60b7e1
RAHUL TIWARI <97533449+RahulCodeTiwari@users.noreply.github.com>
2025-04-16 23:16:15 +0530

    Update Skill.jsx

--- src/components/Skills/Skill.jsx
+++ src/components/Skills/Skill.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Container, Row, Col, Card} from "react-bootstrap";
-import "../Skills/skill.css";
+import "../Skills/Skill.css";
 import { skillsData } from "../../utils/data";
 
 const Skill = () => {
